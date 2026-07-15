a1bee3aec947e452851d5ffef12c47ed0188b47e
UNICO Products <info@unicoproductsindia.com>
2026-07-15 21:01:13 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/nsp-attendance/sw.js', { scope: '/nsp-attendance/' })})}
