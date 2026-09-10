bf173c5a95145e7ec163e77fc4a99d6c3039ec80
nishant mittal <nspenterprises24@gmail.com>
2026-09-10 22:43:52 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/nsp-attendance/sw.js', { scope: '/nsp-attendance/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/attendance/sw.js', { scope: '/attendance/' })})}
